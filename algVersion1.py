c3bc6d0546be9cc813bc0ce21c9e6853b2e7fede
afreedituttu <afreedisulfiker@gmail.com>
2023-11-10 23:53:43 +0530

    version 2

--- algVersion1.py
+++ algVersion1.py
@@ -1,25 +1,32 @@
 import json
 
-def converter(filePath, resultFilePath):
-    with open(filePath, "r") as file:
-        global datas
-        datas = json.load(file)
-    converted_list = []
-    datas = datas["result"]
-    for data in datas:
-        schema = {
-            "title":data.get("name"),
-            "desc":data.get("description",{}).get("short"),
-            "tags":[
-                {
-                    "title":"website",
-                    "url":data.get("domain")
-                },
-            ]
-        }
-        converted_list.append(schema)
-    with open(resultFilePath, "w") as file:
-        json.dump(converted_list, file)
-    return converted_list
+def converter(input_files, result_file):
+    final_list = []
+    for input_file in input_files:
+        with open(input_file, "r") as file:
+            global datas
+            datas = json.load(file)
+        converted_list = []
+        datas = datas["result"]
+        for data in datas:
+            schema = {
+                "title":data.get("name"),
+                "desc":data.get("description",{}).get("short"),
+                "tags":[
+                    {
+                        "title":"website",
+                        "url":data.get("domain")
+                    }
+                ]
+            }
+            converted_list.append(schema)
+        for data in converted_list:
+            final_list.append(data)
+    with open(result_file, "w") as file:
+        json.dump(final_list, file)
+    return final_list
 
-print(converter(r"C:\Users\afree\Desktop\python\1080.json", r"C:\Users\afree\Desktop\python\result.json"))
+input_files = [r"C:\Users\afree\Desktop\python\1020.json", r"C:\Users\afree\Desktop\python\1260.json"]
+result_file = r"./result.json"
+
+converter(input_files, result_file)    
