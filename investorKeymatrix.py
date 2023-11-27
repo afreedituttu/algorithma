9cd53d91939460f97be859945ba893db2c401c04
afreedituttu <afreedisulfiker@gmail.com>
2023-11-27 22:28:41 +0530

    version 1

---
+++ investorKeymatrix.py
@@ -0,0 +1,39 @@
+import json
+
+keymatrices = []
+
+input_files = ['./oxyzo invest/alpha wave/companies.json']
+result_file = './ivnestorKeymatrix.json'
+
+for input_file in input_files:
+    with open(input_file) as file:
+        datas = json.load(file)
+    for data in datas:
+        schema = [
+            {
+                "title":"id",
+                "value":data.get("domainProfileId","")
+            },
+            {
+                "title":"Email id",
+                "value":data.get("emailList",{})[0].get("email","")
+            },
+            {
+                "title":"Website",
+                "value":data.get("domain","")
+            },
+            {
+                "title":"Founded Year",
+                "value":data.get("foundedYear","")
+            },
+            {
+                "title":"Location",
+                "value":data.get("location",{}).get("city","")
+            }
+        ]
+        keymatrices.append(schema)
+
+with open(result_file, 'w') as file:
+    json.dump(keymatrices, file)
+
+print(f'DONE, RESULT STORED TO FILE - {result_file}')
